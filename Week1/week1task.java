cb1d2c40e111bf30276141ec6d383f396e5cb581
anjal <anjalmani2023@gmail.com>
2024-05-03 14:19:06 +0545

    week 2 task

--- Week1/week1task.java
+++ Week1/week1task.java
@@ -47,6 +47,7 @@ A System.out.println statement has ( and ) and usually a String that starts and 
 (But we type \" instead!)
 What is the difference between
 a ' and a "?  Or between a " and a \"?
+a ' and a "?  Or between a " and a \"?
 One is what we see when we're typing our program. The other is what appears on the "console." */
     System.out.println("A well-formed Java program has a main method with { and } braces.");
     System.out.println("A System.out.println statement has ( and ) and usually a String that starts and ends with a \" character.");
